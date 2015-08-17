8a769a6626f2029cb3f3337889df0b1627d6219d
bg2bkk <bg2bkk@gmail.com>
2015-08-17 11:27:04 +0800

    code style

--- lib/abtesting/utils/init.lua
+++ lib/abtesting/utils/init.lua
@@ -4,38 +4,37 @@ local _M = {}
 _M._VERSION = '0.0.1'
 
 _M.redisConf = {
-	["uds"]	     = ngx.var.redis_uds   ,
-	["host"]     = ngx.var.redis_host,
-	["port"]     = ngx.var.redis_port,
-	["poolsize"] = ngx.var.redis_pool_size,
-	["idletime"] = ngx.var.redis_keepalive_timeout , 
-	["timeout"]  = ngx.var.redis_connect_timeout,
-	["dbid"]	 = ngx.var.redis_dbid,
+    ["uds"]      = ngx.var.redis_uds   ,
+    ["host"]     = ngx.var.redis_host,
+    ["port"]     = ngx.var.redis_port,
+    ["poolsize"] = ngx.var.redis_pool_size,
+    ["idletime"] = ngx.var.redis_keepalive_timeout , 
+    ["timeout"]  = ngx.var.redis_connect_timeout,
+    ["dbid"]     = ngx.var.redis_dbid,
 }
 
 _M.divtypes = {
-	["iprange"]     = 'ipParser',  
-	["uidrange"]    = 'uidParser',
-	["uidsuffix"]   = 'uidParser',
-	["uidappoint"]  = 'uidParser',
-    ["url"]         = 'urlParser',
+    ["iprange"]     = 'ipParser',  
+    ["uidrange"]    = 'uidParser',
+    ["uidsuffix"]   = 'uidParser',
+    ["uidappoint"]  = 'uidParser',
 }
 
 _M.prefixConf = {
-	["policyLibPrefix"]		= ngx.var.policy_prefix,
-	["runtimeInfoPrefix"]	= ngx.var.runtime_prefix,
-	["domainname"]			= ngx.var.server_name,
+    ["policyLibPrefix"]     = ngx.var.policy_prefix,
+    ["runtimeInfoPrefix"]   = ngx.var.runtime_prefix,
+    ["domainname"]          = ngx.var.server_name,
 }
 
 _M.divConf = {
-	["default_backend"]		= ngx.var.default_backend;
-	["shdict_expire"]		= ngx.var.shdict_expire;
+    ["default_backend"]     = ngx.var.default_backend;
+    ["shdict_expire"]       = ngx.var.shdict_expire;
 }
 
 _M.cacheConf = {
-	['timeout']			=	ngx.var.lock_expire,
-	['runtimeInfoLock']	=	ngx.var.rt_cache_lock,
-	['upstreamLock']	=	ngx.var.up_cache_lock,
+    ['timeout']             = ngx.var.lock_expire,
+    ['runtimeInfoLock']     = ngx.var.rt_cache_lock,
+    ['upstreamLock']        = ngx.var.up_cache_lock,
 }
 
 return _M
--- lib/abtesting/utils/utils.lua
+++ lib/abtesting/utils/utils.lua
@@ -6,44 +6,44 @@ local cjson = require('cjson.safe')
 --将doresp和dolog，与handler统一起来。handler将返回一个table，结构为：
 --[[
 handler———errinfo————errcode————code
-    |       |               |
-    |       |               |————info
-    |       |
-    |       |————errdesc
+    |           |               |
+    |           |               |————info
+    |           |
+    |           |————errdesc
     |
     |
     |
     |———errstack				 
 ]]--		
 _M.dolog = function(info, desc, data, errstack)
-	local errlog = ''
-	local code, err = info[1], info[2]
-	local errcode = code
-	local errinfo = desc and err..desc or err 
-
-	errlog = errlog .. ' errcode : '..errcode
-	errlog = errlog .. ', errinfo : '..errinfo
-	if data then
-		errlog = errlog .. ', extrainfo : '..data
-	end
-	if errstack then
-		errlog = errlog .. ', errstack : '..errstack
-	end
-	ngx.log(ngx.ERR, errlog)
+    local errlog = ''
+    local code, err = info[1], info[2]
+    local errcode = code
+    local errinfo = desc and err..desc or err 
+    
+    errlog = errlog .. ' errcode : '..errcode
+    errlog = errlog .. ', errinfo : '..errinfo
+    if data then
+        errlog = errlog .. ', extrainfo : '..data
+    end
+    if errstack then
+        errlog = errlog .. ', errstack : '..errstack
+    end
+    ngx.log(ngx.ERR, errlog)
 end
 
 _M.doresp = function(info, desc, data)
-	local response = {}
-
-	local code = info[1]
-	local err  = info[2]
-	response.errcode = code
-	response.errinfo = desc and err..desc or err 
-	if data then 
-		response.data = data 
-	end
-
-	return cjson.encode(response)
+    local response = {}
+    
+    local code = info[1]
+    local err  = info[2]
+    response.errcode = code
+    response.errinfo = desc and err..desc or err 
+    if data then 
+        response.data = data 
+    end
+    
+    return cjson.encode(response)
 end
 
 return _M
