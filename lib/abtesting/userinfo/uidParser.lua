17507bd8942fdecfe8dd63cd04f6797d31c195b6
bg2bkk <bg2bkk@gmail.com>
2015-08-17 11:15:25 +0800

    code style

--- lib/abtesting/userinfo/uidParser.lua
+++ lib/abtesting/userinfo/uidParser.lua
@@ -1,11 +1,10 @@
 
---local user = require "weibo.ban.user"
 local _M = {
     _VERSION = '0.01'
 }
 
 _M.get = function()
-	local u = ngx.req.get_headers()["X-Log-Uid"]
+	local u = ngx.req.get_headers()["X-Uid"]
 	return u
 end
 return _M
