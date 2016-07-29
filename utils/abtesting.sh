d4b683a8dc83458ff0ac2376acda5432e1294aed
bg2bkk <bg2bkk@gmail.com>
2016-07-29 19:48:46 +0800

    remove directive stub_status

--- utils/abtesting.sh
+++ utils/abtesting.sh
@@ -2,9 +2,9 @@
 
 rm *temp -rf
 
-/home/huang/workspace/tengine-2.1.2/objs/nginx -p `pwd` -c conf/nginx.conf  
-/home/huang/workspace/tengine-2.1.2/objs/nginx -p `pwd` -c conf/stable.conf 
-/home/huang/workspace/tengine-2.1.2/objs/nginx -p `pwd` -c conf/beta1.conf  
-/home/huang/workspace/tengine-2.1.2/objs/nginx -p `pwd` -c conf/beta2.conf  
-/home/huang/workspace/tengine-2.1.2/objs/nginx -p `pwd` -c conf/beta3.conf  
-/home/huang/workspace/tengine-2.1.2/objs/nginx -p `pwd` -c conf/beta4.conf  
+/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/nginx.conf  
+/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/stable.conf 
+/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta1.conf  
+/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta2.conf  
+/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta3.conf  
+/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta4.conf  
